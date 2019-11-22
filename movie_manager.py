abcb67a4cb1137d0952c70624fc9cfbc40816d9f
a <a>
2019-11-22 11:32:40 +0000

    Add support to register movies.

--- movie_manager.py
+++ movie_manager.py
@@ -11,4 +11,24 @@ def registar_ator(mm, nome_ator):
     mm['atores'].append(ator)
 
 def registar_filme(mm, titulo, genero, nome_realizador):
-    pass
+    # Pesquisar realizador com base no nome
+    realizador = None
+    for r in mm['realizadores']:
+        if r['nome'] == nome_realizador:
+            realizador = r
+            break
+    if realizador is None:
+        # Se o realizador não existir, falha
+        return None
+    else:
+        # Se o realizador existir, adiciona filme
+        filme = {
+            'titulo': titulo,
+            'genero': genero,
+            'rating': 0.0,
+            'sinopse': "",
+            'realizador': realizador,
+            'atores': []
+        }
+        mm['filmes'].append(filme)
+        return filme
--- program.py
+++ program.py
@@ -11,6 +11,9 @@ def main():
     print(mm)
     movie_manager.registar_ator(mm, 'João')
     print(mm)
+    movie_manager.registar_filme(mm, "Jurasic Park", "Ação", "Spielberg")
+    print()
+    print(mm)
 
 if __name__ == "__main__":
     main()
