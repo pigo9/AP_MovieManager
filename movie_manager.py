4edf739eefab75e14ce8fafdb11ba0a34c36a20c
André Sabino <andre.sabino@gmail.com>
2019-11-27 09:11:06 +0000

    Add RR command to parser.

--- movie_manager.py
+++ movie_manager.py
@@ -1,34 +1,5 @@
-def registar_realizador(mm, nome_realizador):
-    realizador = {
-        'nome': nome_realizador
-    }
-    mm['realizadores'].append(realizador)
+def has_director(mm, name):
+    pass
 
-def registar_ator(mm, nome_ator):
-    ator = {
-        'nome': nome_ator
-    }
-    mm['atores'].append(ator)
-
-def registar_filme(mm, titulo, genero, nome_realizador):
-    # Pesquisar realizador com base no nome
-    realizador = None
-    for r in mm['realizadores']:
-        if r['nome'] == nome_realizador:
-            realizador = r
-            break
-    if realizador is None:
-        # Se o realizador não existir, falha
-        return None
-    else:
-        # Se o realizador existir, adiciona filme
-        filme = {
-            'titulo': titulo,
-            'genero': genero,
-            'rating': 0.0,
-            'sinopse': "",
-            'realizador': realizador,
-            'atores': []
-        }
-        mm['filmes'].append(filme)
-        return filme
+def add_director(mm, name):
+    pass
--- program.py
+++ program.py
@@ -1,19 +1,19 @@
-import movie_manager
+import movie_manager as mm
 
 def main():
-    mm = {
-        'realizadores': [],
-        'atores': [],
-        'filmes': []
-    }
-    print(mm)
-    movie_manager.registar_realizador(mm, 'Spielberg')
-    print(mm)
-    movie_manager.registar_ator(mm, 'João')
-    print(mm)
-    movie_manager.registar_filme(mm, "Jurasic Park", "Ação", "Spielberg")
-    print()
-    print(mm)
+    movie_manager_D = {}
+    while True:
+        line = input()
+        
+        commands = input.split(" ")
+
+        if commands[0] == "RR":
+            name = commands[1]
+            if mm.has_director(movie_manager_D, name):
+                print("Realizador existente.")
+            else:
+                mm.add_director(movie_manager_D, name)
+                print("Realizador registado com sucesso.")
 
 if __name__ == "__main__":
     main()
