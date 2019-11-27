928dd2ba58a9459fc5b52591e30c880a15d11980
André Sabino <andre.sabino@gmail.com>
2019-11-27 09:16:24 +0000

    Refactor RR command into function.

--- program.py
+++ program.py
@@ -8,12 +8,15 @@ def main():
         commands = input.split(" ")
 
         if commands[0] == "RR":
-            name = commands[1]
-            if mm.has_director(movie_manager_D, name):
-                print("Realizador existente.")
-            else:
-                mm.add_director(movie_manager_D, name)
-                print("Realizador registado com sucesso.")
+            commandRR(commands, movie_manager_D)
+
+def commandRR(commands, movie_manager_D):
+    name = commands[1]
+    if mm.has_director(movie_manager_D, name):
+        print("Realizador existente.")
+    else:
+        mm.add_director(movie_manager_D, name)
+        print("Realizador registado com sucesso.")
 
 if __name__ == "__main__":
     main()
